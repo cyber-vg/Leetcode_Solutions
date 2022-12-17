d382f7fe2b47d02488aea130ea1de18f045f4eb1
Yaduttam95 <yaduttampareek95@gmail.com>
2022-12-17 08:30:23 +0530

    150. Evaluate Reverse Polish Notation

---
+++ solutions/0101-0200/150. Evaluate Reverse Polish Notation/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+    Stack<Integer> stack = new Stack<>();
+    for (String s : tokens) {
+        if ("+".equals(s)) stack.push(stack.pop() + stack.pop());
+        else if ("-".equals(s)) stack.push(-stack.pop() + stack.pop());
+        else if ("/".equals(s)) stack.push((int)(1D / stack.pop() * stack.pop()));
+        else if ("*".equals(s)) stack.push(stack.pop() * stack.pop());
+        else stack.push(Integer.valueOf(s));
+    }
+
+    return stack.pop();
+}
+}
+
+/*
+ * This approach is using Stack data structure which works on LIFO (Last In First Out) Principle. 
+*/
